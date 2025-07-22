669b3c3ba9bc8d2b41a6b014288a60b05bbd3207
sanjay-sk-99 <sanjay.g8699@gmail.com>
2025-07-22 18:30:58 +0530

    feat:profile-screen-added and profile-image-upload-function-added

--- todo/src/tabs/Profile.jsx
+++ todo/src/tabs/Profile.jsx
@@ -1,15 +1,69 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Linking,
+  Image,
+  Alert,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { CommonActions } from "@react-navigation/native";
-import React from "react";
+import profile from "../../assets/profile.png";
 
 const Profile = ({ navigation }) => {
+  const [profileImage, setProfileImage] = useState(null);
+
+  // Handle external link
+  const handleExternelLink = (url) => {
+    Linking.openURL(url);
+  };
+
+  // Open camera to take photo
+  const takePhoto = async () => {
+    const { status } = await ImagePicker.requestCameraPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Permission Denied", "Camera access is required.");
+      return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setProfileImage(result.assets[0].uri);
+    }
+  };
+
+  //choose photo from gallery
+  const pickImageFromGallery = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert("Permission Denied", "Gallery access is required.");
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setProfileImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.btncontainer}>
         <TouchableOpacity
           style={styles.loginbtn}
           onPress={() =>
-            //it will remove previous screen on the stack
             navigation.dispatch(
               CommonActions.reset({
                 index: 0,
@@ -18,8 +72,59 @@ const Profile = ({ navigation }) => {
             )
           }
         >
-          <Text style={styles.btntext}>Login</Text>
+          <Text style={styles.btntext}>Logout</Text>
         </TouchableOpacity>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.profileImageContainer}>
+          {profileImage ? (
+            <Image source={{ uri: profileImage }} style={styles.profileImage} />
+          ) : (
+            <Image source={profile} style={{ height: 100, width: 100 }} />
+          )}
+          <View style={{ flexDirection: "row" }}>
+            <TouchableOpacity style={styles.imageButton} onPress={takePhoto}>
+              <Text style={styles.imageButtonText}>Take Photo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.imageButton}
+              onPress={pickImageFromGallery}
+            >
+              <Text style={styles.imageButtonText}>Choose Gallery</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* User Info */}
+        <View style={styles.userInfo}>
+          <Text style={styles.infoText}>Name : Sanjay Kumar</Text>
+          <Text style={styles.infoText}>Gmail : sanjay@gmail.com</Text>
+          <Text style={styles.infoText}>Phone No : 1234567890</Text>
+
+          <View style={styles.linkContainer}>
+            <Text style={styles.infoText}>GitHub profile : </Text>
+            <TouchableOpacity
+              onPress={() =>
+                handleExternelLink("https://github.com/sanjay-sk-99")
+              }
+            >
+              <Text style={styles.link}>Click Here</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.linkContainer}>
+            <Text style={styles.infoText}>LinkedIn profile : </Text>
+            <TouchableOpacity
+              onPress={() =>
+                handleExternelLink(
+                  "https://www.linkedin.com/in/99-sanjay-kumar/"
+                )
+              }
+            >
+              <Text style={styles.link}>Click Here</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -49,5 +154,51 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 15,
     color: "#fff",
+  },
+  profileImageContainer: {
+    marginVertical: 20,
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 10,
+  },
+  imageButton: {
+    backgroundColor: "#007AFF",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+  imageButtonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  link: {
+    color: "blue",
+    textDecorationLine: "underline",
+  },
+  linkContainer: {
+    flexDirection: "row",
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 100,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  userInfo: {
+    marginTop: 10,
+  },
+  infoText: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "bold",
   },
 });
